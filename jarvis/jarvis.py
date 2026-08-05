f36c22457c1119b57c54f432b28110e20d80c96d
Kootryne <viktor.sunnanbo@gmail.com>
2026-08-06 00:29:29 +0200

    Load final conversation reliability fixes for 0.8.6

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -6,6 +6,7 @@ from jarvis_app.enhancements_v083 import apply_patches as apply_v083_patches
 from jarvis_app.reliability_v084 import apply_patches as apply_v084_patches
 from jarvis_app.reliability_v085 import apply_patches as apply_v085_patches
 from jarvis_app.reliability_v086 import apply_patches as apply_v086_patches
+from jarvis_app.conversation_reliability_v086 import apply_patches as apply_conversation_v086_patches
 
 apply_process_patches()
 apply_v08_patches()
@@ -15,6 +16,7 @@ apply_v083_patches()
 apply_v084_patches()
 apply_v085_patches()
 apply_v086_patches()
+apply_conversation_v086_patches()
 
 from jarvis_app.main import main
 

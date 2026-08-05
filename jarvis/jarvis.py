42d7bb52ecbb96a2b8cbe52ca07fec667d26f385
Kootryne <viktor.sunnanbo@gmail.com>
2026-08-06 00:04:31 +0200

    Load Jarvis 0.8.5 reliability patches

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -4,6 +4,7 @@ from jarvis_app.enhancements_v081 import apply_patches as apply_v081_patches
 from jarvis_app.reliability_v082 import apply_patches as apply_v082_patches
 from jarvis_app.enhancements_v083 import apply_patches as apply_v083_patches
 from jarvis_app.reliability_v084 import apply_patches as apply_v084_patches
+from jarvis_app.reliability_v085 import apply_patches as apply_v085_patches
 
 apply_process_patches()
 apply_v08_patches()
@@ -11,6 +12,7 @@ apply_v081_patches()
 apply_v082_patches()
 apply_v083_patches()
 apply_v084_patches()
+apply_v085_patches()
 
 from jarvis_app.main import main
 

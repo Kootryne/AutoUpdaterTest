6cc88bfabb9ffbdb2262a64f598f6c2a445cb513
Kootryne <viktor.sunnanbo@gmail.com>
2026-08-06 00:25:25 +0200

    Load Jarvis 0.8.6 model, TTS, and skill reliability patches

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -5,6 +5,7 @@ from jarvis_app.reliability_v082 import apply_patches as apply_v082_patches
 from jarvis_app.enhancements_v083 import apply_patches as apply_v083_patches
 from jarvis_app.reliability_v084 import apply_patches as apply_v084_patches
 from jarvis_app.reliability_v085 import apply_patches as apply_v085_patches
+from jarvis_app.reliability_v086 import apply_patches as apply_v086_patches
 
 apply_process_patches()
 apply_v08_patches()
@@ -13,6 +14,7 @@ apply_v082_patches()
 apply_v083_patches()
 apply_v084_patches()
 apply_v085_patches()
+apply_v086_patches()
 
 from jarvis_app.main import main
 

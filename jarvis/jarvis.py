d00e86d99fb59de91382aea2faa00c834964c5c3
Kootryne <viktor.sunnanbo@gmail.com>
2026-08-06 10:33:30 +0200

    Load Luna-driven control layer

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -10,6 +10,7 @@ from jarvis_app.conversation_reliability_v086 import apply_patches as apply_conv
 from jarvis_app.reliability_v087 import apply_patches as apply_v087_patches
 from jarvis_app.reliability_v088 import apply_patches as apply_v088_patches
 from jarvis_app.reliability_v089 import apply_patches as apply_v089_patches
+from jarvis_app.reliability_v090 import apply_patches as apply_v090_patches
 
 apply_process_patches()
 apply_v08_patches()
@@ -23,6 +24,7 @@ apply_conversation_v086_patches()
 apply_v087_patches()
 apply_v088_patches()
 apply_v089_patches()
+apply_v090_patches()
 
 from jarvis_app.main import main
 

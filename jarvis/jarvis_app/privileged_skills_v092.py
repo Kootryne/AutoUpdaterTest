12c73ed23fc959cfa4e631427f6e019e33224bcb
Kootryne <viktor.sunnanbo@gmail.com>
2026-08-06 12:25:27 +0200

    Preserve all existing skill tools after permission patch

--- jarvis/jarvis_app/privileged_skills_v092.py
+++ jarvis/jarvis_app/privileged_skills_v092.py
@@ -10,7 +10,17 @@ def apply_patches() -> None:
 
     from .skill_builder_v092 import patch_builder_and_runtime
     from .skill_approval_v092 import patch_skill_approval_flow
+    from .skills import SkillSystem
 
     patch_builder_and_runtime()
     patch_skill_approval_flow()
+
+    prior_handles = SkillSystem.handles_tool
+
+    def handles_every_published_skill_tool(self: SkillSystem, name: str) -> bool:
+        if prior_handles(self, name):
+            return True
+        return any(schema.get("name") == name for schema in self.schemas())
+
+    SkillSystem.handles_tool = handles_every_published_skill_tool
     _PATCHED = True

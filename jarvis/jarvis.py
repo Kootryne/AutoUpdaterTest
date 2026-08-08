16e0429980e263aca0528c16a14d5ecb2a319ead
Kootryne <viktor.sunnanbo@gmail.com>
2026-08-08 18:12:51 +0200

    Load adaptive gap guard last

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -23,6 +23,7 @@ from jarvis_app.update_bootstrap_v097 import apply_patches as apply_update_boots
 from jarvis_app.self_modification_v098 import apply_patches as apply_self_modification_v098_patches
 from jarvis_app.adaptive_core_v099 import apply_patches as apply_adaptive_core_v099_patches
 from jarvis_app.self_modification_dependency_fix_v0910 import apply_patches as apply_self_mod_dependency_fix_v0910_patches
+from jarvis_app.adaptive_gap_guard_v0911 import apply_patches as apply_adaptive_gap_guard_v0911_patches
 
 apply_process_patches()
 apply_v08_patches()
@@ -49,6 +50,7 @@ apply_update_bootstrap_v097_patches()
 apply_self_modification_v098_patches()
 apply_adaptive_core_v099_patches()
 apply_self_mod_dependency_fix_v0910_patches()
+apply_adaptive_gap_guard_v0911_patches()
 
 from jarvis_app.main import main
 
